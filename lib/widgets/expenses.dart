e75d014a67823a9d3a170bb948864e2ec6a511ba
ikramzargar <ikramxargar@gmail.com>
2024-10-12 15:55:43 +0530

    responsive

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -65,6 +65,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+   final width =  MediaQuery.of(context).size.width;
+
     Widget mainContent =
         const Center(child: Text('No expenses found. Start adding some!'));
     if (_registeredExpenses.isNotEmpty) {
@@ -81,9 +83,14 @@ class _ExpensesState extends State<Expenses> {
               onPressed: _addExpenseOverlay, icon: const Icon(Icons.add)),
         ],
       ),
-      body: Column(
+      body: width < 600 ? Column(
         children: [
          Chart(expenses: _registeredExpenses),
+          Expanded(child: mainContent),
+        ],
+      ) :Row(
+        children: [
+          Expanded(child: Chart(expenses: _registeredExpenses)),
           Expanded(child: mainContent),
         ],
       ),
